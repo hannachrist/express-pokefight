3a9991dd80c668df72fb3b94dffe80eefbd92a3d
Hanna Christ <christh87@googlemail.com>
2020-06-22 15:08:00 +0200

    Add pokemon id and info routes

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
-// const path = require('path');
 
 const pokedex = require('../pokedex.json');
+
+router.get('/:id/:info', function (req, res, next) {
+    const { id, info } = req.params;
+
+    const idAsNumber = parseInt(id, 10);
+
+    const foundPokemon = pokedex.find((singlePokemon) => {
+        return singlePokemon.id === idAsNumber;
+    });
+
+    res.send(foundPokemon[info]);
+})
+
+router.get('/:id', function (req, res, next) {
+    const { id } = req.params;
+
+    const idAsNumber = parseInt(id, 10);
+
+    const foundPokemon = pokedex.find((singlePokemon) => {
+        return singlePokemon.id === idAsNumber;
+    });
+
+    res.send(foundPokemon);
+});
 
 router.get('/', function (req, res, next) {
     res.send(pokedex);
